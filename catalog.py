5b4de0fa3a4911b8c43cadef8aed867eae5954ef
Michael Price <mprice0064@gmail.com>
2019-08-31 14:33:56 -0400

    cleaned up a little

--- catalog.py
+++ catalog.py
@@ -79,5 +79,6 @@ def showCategoryItemDescription(categories_id, item_id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 8000)
